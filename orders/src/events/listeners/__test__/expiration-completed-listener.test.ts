8b8bc5f60a8eaed54595922170e157141ec72e14
Fidelion <david.d.95@hotmail.com>
2021-10-27 11:53:31 +0200

    added await infront of listener in test

--- orders/src/events/listeners/__test__/expiration-completed-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-completed-listener.test.ts
@@ -41,7 +41,7 @@ const setup = async() => {
 it('updates the order to cancelled', async() => {
     const { listener, order, data, msg } = await setup();
 
-    listener.onMessage(data, msg);
+    await listener.onMessage(data, msg);
 
     const updatedOrder = await Order.findById(order.id);
 
@@ -51,7 +51,7 @@ it('updates the order to cancelled', async() => {
 it('emits the OrderCancel event', async() => {
     const { listener, order, data, msg } = await setup();
 
-    listener.onMessage(data, msg);
+    await listener.onMessage(data, msg);
 
     expect(natsClient.client.publish).toHaveBeenCalled();
 
@@ -63,7 +63,7 @@ it('emits the OrderCancel event', async() => {
 it('acks the event function', async() => {
     const { listener, data, msg } = await setup();
 
-    listener.onMessage(data, msg);
+    await listener.onMessage(data, msg);
 
     expect(msg.ack).toHaveBeenCalled();
 });
