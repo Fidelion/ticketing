0961bd0c6b2d7c8c5547562ffb662a38123aba3e
Fidelion <david.d.95@hotmail.com>
2021-10-27 12:15:04 +0200

    removed env variable from test new route in payments

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -8,7 +8,7 @@ import { stripe } from "../../stripe";
 
 // jest.mock('../../stripe');
 
-process.env.STRIPE_KEY = "sk_test_51GcSvuCyYW8raZL5Yu93gSmqLH7S3TT4zyMhtzgU0ot6zAo9e0XRJNz1ZiLIXcvApsrxJMHTjQ0NpFykLvv4tjSQ0088k88eOa";
+// process.env.STRIPE_KEY = "sk_test_51GcSvuCyYW8raZL5Yu93gSmqLH7S3TT4zyMhtzgU0ot6zAo9e0XRJNz1ZiLIXcvApsrxJMHTjQ0NpFykLvv4tjSQ0088k88eOa";
 
 it('returns 404 when purchasing an order that does not exist', async() => {
     await request(app).post('/api/payments')
