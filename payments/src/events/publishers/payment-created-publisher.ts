94b883ec661d80d5637380275201062500509131
Fidelion <david.d.95@hotmail.com>
2021-10-27 12:59:55 +0200

    added publish mock function within payment created publisher

--- payments/src/events/publishers/payment-created-publisher.ts
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -2,4 +2,7 @@ import { Subjects, Publisher, PaymentCreatedEvent } from "@ticketsnode/common";
 
 export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
     subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    publish = jest.fn().mockImplementation(({}) => {
+        return Promise.resolve({});
+    });
 }
